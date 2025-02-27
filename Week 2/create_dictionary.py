bcf8eacca32848f8b3ace7c6c326f8921f87fdca
Nandika K <2nandika.k@gmail.com>
2025-02-26 20:24:44 -0500

    problem 2

--- Week 2/create_dictionary.py
+++ Week 2/create_dictionary.py
@@ -1,5 +1,8 @@
 def create_dictionary(keys, values):
-    pass
+    dict = {}
+    for i in range (len(keys)):
+        dict[keys[i]] = values[i]
+    return dict
 
 keys = ["peanut", "dragon", "star", "pop", "space"]
 values = ["butter", "fly", "fish", "corn", "ship"]
